c48b7a2a3fc0632ab529f00c5f087f03deb7b19d
Kartavya <kartavyasharma123456@gmail.com>
2023-05-24 18:59:06 +0530

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Header from "./Components/Header.svelte";
+	import Meme from "./Components/Meme.svelte"
+</script>
+
+<main>
+	<Header/>
+	<Meme/>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Components/Header.svelte
@@ -0,0 +1,14 @@
+<script>
+import Face from "./troll.png"
+</script>
+
+<main>
+<div class="header"></div>
+<img src = {Face} alt = "smiling-face"/>
+<h1>Meme Generator</h1>
+</main>
+
+<style>
+
+
+</style>
